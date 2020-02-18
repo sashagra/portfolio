7188d78100aeef6e1d05b37da737cf9881e85211
archon.san@gmail.com <archon.san@gmail.com>
2020-02-18 15:48:17 +0300

    add AJAX >> google-form

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// swiper slider
+
 new WOW().init();
 var mySwiper = new Swiper('.swiper-container', {
     speed: 400,
@@ -12,3 +14,36 @@ var mySwiper = new Swiper('.swiper-container', {
         clickable: true,
       },
 });
+
+// Writing utm 2 input field
+
+const utmField = document.querySelector('#utm');
+utmField.value = document.location.href;
+
+// AJAX >> google form
+
+!function(exports) {
+  exports.submitGoogleForm = submitGoogleForm;
+
+  function submitGoogleForm(form) {
+    try {
+      var data = [].slice.call(form).map(function(control) {
+        return 'value' in control && control.name ?
+          control.name + '=' + (control.value === undefined ? '' : control.value) :
+          '';
+      }).join('&');
+      var xhr = new XMLHttpRequest();
+
+      xhr.open('POST', form.action + '/formResponse', true);
+      xhr.setRequestHeader('Accept',
+          'application/xml, text/xml, */*; q=0.01');
+      xhr.setRequestHeader('Content-type',
+          'application/x-www-form-urlencoded; charset=UTF-8');
+      xhr.send(data);
+    } catch(e) {}
+
+    form.parentNode.className += ' submitted';
+
+    return false;
+  }
+}(typeof module === 'undefined' ? window : module.exports);
